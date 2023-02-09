87871fd7703cd051a8a26235ab863f3ef196a289
otavio <otaviogrocha@yahoo.com.br>
2023-02-09 16:48:24 -0300

    starting crud js

--- README.md
+++ README.md
@@ -1,5 +1,7 @@
 # CRUD-WEB-JS
 
 - Arrumar o botão de #ADD [x]
-- Fazer funções CRUD []
-- Colocar diretório .vscode dentro do git ignore e remover do repositório remoto []
+- Colocar diretório .vscode/ dentro do git ignore e remover do repositório remoto [x]
+- Fazer funções CRUD [ ]
+- Alterar tipo de input no form de Adicionar Aluno[ ]
+- Gerar número de ID toda vez que um usuário for adicionado[ ]
--- src/js/main.js
+++ src/js/main.js
@@ -1,8 +1,57 @@
+const save = document.querySelector('.save');
+const modal = document.querySelector('.modal-container');
+const tbody = document.querySelector('.body-table');
+const name = document.getElementById('#name');
+const turma = document.getElementById('#turma');
+const idade = document.getElementById('#idade');
+
+const elements = {
+    id: 0,
+    name: "otavio",
+    turma: 1,
+}
+
+
+insertValues = () =>{
+
+let tr = document.createElement('tr')
+
+tr.innerHTML = `
+<td>{input.nome}</td>
+<td>{input.turma}</td>
+<td>{input.idade}</td>
+<td class ="acao">
+    <button onclick = "editItem({})"><i class ='bx bx-edit'></i></button>
+</td>
+<td class ="acao">
+    <button onclick = "deleteItem({})"><i class ='bx bx-trash'></i></button>
+</td>
+`
+tbody.appendChild(tr)
+
+}
+
+function iterator (array){
+    array.forEach(item => {
+        console.log(item)
+    });
+}
+
+
+//  Ativar e remover Modal
+
 
 
 // CREATE
 
+save.addEventListener('onclick', () =>{
+insertValues();
 
+
+
+    
+modal.classList.remove('.active')
+})
 
 
 // READ
@@ -16,3 +65,9 @@
 
 
 // DELETE
+
+
+
+
+
+
