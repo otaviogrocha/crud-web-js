9af99b87dd2fbb91832dc3c4f0dcd4d4688ed26e
otavio <otaviogrocha@yahoo.com.br>
2023-02-14 16:42:13 -0300

    function to open modal 100%, starting the CREATE func to insert content into HTML + variables from html fix & insert value started

--- src/js/main.js
+++ src/js/main.js
@@ -1,65 +1,77 @@
-const save = document.querySelector('.save');
+const saveBtn = document.querySelector('#save');
+const addBtn = document.getElementById('add');
 const modal = document.querySelector('.modal-container');
-const tbody = document.querySelector('.body-table');
-const name = document.getElementById('#name');
-const turma = document.getElementById('#turma');
-const idade = document.getElementById('#idade');
+const tbody = document.querySelector('tbody');
+const nome = document.getElementById('name');
+const turma = document.getElementById('turma');
+const idade = document.getElementById('idade');
 
-const elements = {
-    id: 0,
-    name: "otavio",
-    turma: 1,
+// const elements = {
+//     id: 0,
+//     name: "otavio",
+//     turma: 1,
+// }
+
+
+function insertValues(){
+    let tr = document.createElement('tr')
+
+    tr.innerHTML = `
+        <td>${nome.value}</td>
+        <td>${turma.value}</td>
+        <td>${idade.value}</td>
+        <td class ="acao">
+            <button onclick = "editItem({})"><i class ='bx bx-edit'></i></button>
+        </td>
+        <td class ="acao">
+            <button onclick = "deleteItem({})"><i class ='bx bx-trash'></i></button>
+        </td>
+    `
+    tbody.appendChild(tr)
 }
 
-
-insertValues = () =>{
-
-let tr = document.createElement('tr')
-
-tr.innerHTML = `
-<td>{input.nome}</td>
-<td>{input.turma}</td>
-<td>{input.idade}</td>
-<td class ="acao">
-    <button onclick = "editItem({})"><i class ='bx bx-edit'></i></button>
-</td>
-<td class ="acao">
-    <button onclick = "deleteItem({})"><i class ='bx bx-trash'></i></button>
-</td>
-`
-tbody.appendChild(tr)
-
-}
-
-function iterator (array){
-    array.forEach(item => {
-        console.log(item)
-    });
-}
+// function iterator(array) {
+//     array.forEach(item => {
+//         console.log(item)
+//     });
+// }
 
 
 //  Ativar e remover Modal
 
+function openModal() {
+    addBtn.addEventListener('click', () => {
+        modal.classList.add('active')
+    })
 
+    document.addEventListener('click', (e) => {
+        if (e.target.className == 'modal-container active')
+            modal.classList.remove('active');
+    })
+}
 
+openModal();
 // CREATE
 
-save.addEventListener('onclick', () =>{
-insertValues();
+function create() {
+    saveBtn.addEventListener('click', (e) => {
 
+        e.preventDefault()
+        insertValues();
+        modal.classList.remove('active')
+        console.log('clique')
+    }
+    )
+}
 
-
-    
-modal.classList.remove('.active')
-})
-
+create()
 
 // READ
 
 
 
 
-// UPDATE 
+// UPDATE
 
 
 
